1df168ec240bca4047c7ae430d0e18831e1abfe6
SleepyMode <sleepy@sleepymode.me>
2024-03-21 23:17:41 +0200

    More user class memery

--- src/core/models/user.js
+++ src/core/models/user.js
@@ -1,31 +1,78 @@
 import {Database} from '../../lib/database/database.js';
 
 export class User {
-    static findByUniqueId(uniqueId) {
+    static async findByUniqueId(uniqueId) {
+        const result = await Database.select('sb_users')
+            .where('uid', uniqueId)
+            .execute()
+            .catch();
+
+        if (result == null || result.length <= 0) {
+            return null;
+        }
+
+        const userData = result[0];
+        let user = new User();
+        user.uniqueId = userData.uid;
+        await user.fetch();
+        return user;
     }
 
-    static findByUsername(username) {
+    static async findByUsername(username, exactMatch = false) {
+        let result;
+        if (exactMatch) {
+            result = await Database.select('sb_users')
+                .where('username', username)
+                .execute()
+                .catch();
+        } else {
+            result = await Database.select('sb_users')
+                .whereLike('username', username)
+                .execute()
+                .catch();
+        }
+
+        if (result == null || result.length <= 0) {
+            return null;
+        }
+
+        // Not calling fetch here to avoid multiple db calls.
+        const userData = result[0];
+        let user = new User();
+        user.uniqueId = userData.uid;
+        user.username = userData.username;
+        return user;
     }
 
     uniqueId;
     username;
 
-    constructor(uniqueId) {
-        Database.select('sb_users')
-            .where('uid', uniqueId)
-            .setCallback((err, res, fields) => {
-                console.log(err);
-                console.log('!!!!!!');
-                console.log(res);
-                console.log('!!!!!!');
-                console.log(fields);
-            })
+    async push() {
+        // TODO: Error handling
+        await Database.update('sb_users')
+            .whereLike('uid', this.uniqueId)
+            .update('username', this.username)
             .execute();
     }
 
+    async fetch() {
+        const data = await Database.select('sb_users')
+            .where('uid', this.uniqueId)
+            .execute()
+            .catch();
+
+        if (data == null || data.length <= 0) {
+            return;
+        }
+
+        this.username = data[0].username;
+    }
+
     getUsername() {
+        return this.username;
     }
 
     setUsername(username) {
+        this.username = username;
     }
 }
